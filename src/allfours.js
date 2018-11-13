a499fcd811e71c95d227a98716df6ab922a2bf71
muddiman <muddiman@hotmail.com>
2018-11-13 19:12:54 -0400

    testing all functions

--- src/allfours.js
+++ src/allfours.js
@@ -4,6 +4,14 @@
                      Programmer: .muddicode                             
 */
 
+/**
+ *  @author: Roger Clarke (muddiman | .muddicode)
+ *      see: https://www.roger-clarke.com
+ *  @license: Dual license - MIT & GPL
+ *       See: http://www.gnu.org/licenses/gpl.html
+ *            http://www.
+ */
+
 /*
    Major Elements of A Game:
    I. Setup Game
@@ -39,22 +47,40 @@
 
 var gameBoard = {
 //  Object: gameBoard
-    canvas : document.getElementById("game_board"),
-    width : 750,
-    height : 450,
+    canvas : document.createElement("canvas"),
+     
     // ctx : this.canvas.getContext("2d"),
     init : function () {
-    // initialize gameBoard by applying context
+    // initialize gameBoard by applying context & inserting the canvas in the "game_container" <div> 
+        this.canvas.width = 700;
+        this.canvas.height = 450;
+        this.canvas.id = "game_board";
         this.ctx = this.canvas.getContext("2d");
+        document.getElementById("game_container").appendChild(this.canvas);
+        // this.frameNo =0;
+        // this.interval = setInterval(updateGameArea, 20);
     },
+    // if 'setInterval is used, there should be stop function
+    // stop : function () {clearInterval(this.interval);}
     clearBoard : function () {
     // wipes the entire gameBoard clean
-        this.ctx.clearRect(0, 0, this.width, this.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 };
 
+function unitTestForGameBoard() {
+// @test: if game board is created in intended place in the DOM
+// @param: null
+// @return: void  
+    gameBoard.init();
+    var x = gameBoard.ctx;
+    x.fillStyle= "#FF0000";
+    x.fillRect(0,0, 150,75);
+    gameBoard.clearBoard();
+}
 
-// var gameWidth = 750;
+
+// var gameWidth = 700;
 // var gameHeight = 450; 
 var xCenter = gameBoard.width/2;
 var yCenter = gameBoard.height/2;
@@ -64,7 +90,7 @@ var yCenter = gameBoard.height/2;
 function startAsk() {
     if (confirm("Click 'OK' to play All Fours or 'Cancel' to exit game.")) {
         game.quit = false;
-        mainLoop();
+        mainGameLoop();
     } else {
         quit = true;
     }
@@ -73,16 +99,34 @@ function startAsk() {
 
 
 function Card(rank, face, suit) {
-// Card object constructor
+// Card object constructor (game components are usually created writing a constructor for each type of component)
     this.suit = suit; // ['c', 'd', 'h', 's'],
     this.face = face; // ['2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'q', 'k', 'a'],
     this.rank = rank;     // [0, 1,.. 12], to assist in determining who played the higher card
     this.getCardName = function () {
         return this.face + this.suit; // used as cardId, image filename, etc
     };
+    this.image = new Image();
+    this.image.id = this.getCardName();
+    this.image.src = "img/" + this.getCardName() + ".png";
+    // write new card.functions as needed, ie update function & location function
+}
+
+function unitTestForCardObjects() {
+// @test: all properties of the Card object are initialized
+// @param: null
+// @return: void
+    var aCard = new Card(0,2,'c');
+    var x = gameBoard.ctx;
+    x.drawImage(aCard.image, 5,5);
+    console.log(aCard.face + aCard.suit);
+    console.log(aCard.rank + "."+aCard.getCardName() + "."+aCard.image.src);
 }
 
 function createDeck() {
+// 
+// @param: null
+// @return: deck
     var suits = ['c', 'd', 'h', 's'];
     var faces = ['2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'q', 'k', 'a']
     var n = 0;
@@ -98,13 +142,21 @@ function createDeck() {
     return deck;
 }
 
-function UnitTestObjects(deck) {
+function unitTestForDeckArray() {
+// @test: the deck-array. All 52 unique card objects, with all their parameters
+// @param: deck (array of Cards)
+// @return: void
+    var deck = createDeck();
     for (i = 0; i < 52; i++) {
-        console.log(i); 
-        //  aCard = deck[i].face + " of " + deck[i].fuit;
-        var aCard = deck[i].getCardName();
-        console.log(aCard);
-    }
+        setTimeout(function () {
+            console.log(i); 
+            var aCard = deck[i];
+            var x = gameBoard.ctx;
+            x.drawImage(aCard.image, 100,100);
+            console.log(aCard.face + aCard.suit);
+            console.log(aCard.rank + "."+aCard.getCardName() + "."+aCard.image.src);
+        }, 1500);
+    };
 }
 
 function cardDisplay() {
--- src/unitTests.js
+++ src/unitTests.js
@@ -4,14 +4,42 @@
 
 */
 
-function UnitTestObjects(deck) {
-// Test the deck-array. All 52 unique card objects, with all their parameters
-    for (i = 0; i < 52; i++) {
-        console.log(i); 
-        //  aCard = deck[i].cardFace + " of " + deck[i].cardSuit;
-        var aCard = deck[i].getCardName();
-        console.log(aCard);
+function unitTestForGameBoard() {
+    // @test: if game board is created in intended place in the DOM
+    // @param: null
+    // @return: void  
+        gameBoard.init();
+        var x = gameBoard.ctx;
+        x.fillStyle= "#FF0000";
+        x.fillRect(0,0, 150,75);
+        gameBoard.clearBoard();
     }
+function unitTestForCardObjects() {
+// @test: all properties of the Card object are initialized
+// @param: null
+// @return: void
+        var aCard = new Card(0,2,'c');
+        var x = gameBoard.ctx;
+        x.drawImage(aCard.image, 5,5);
+        console.log(aCard.face + aCard.suit);
+        console.log(aCard.rank + "."+aCard.getCardName() + "."+aCard.image.src);
+    }
+
+function unitTestForDeckArray() {
+// @test: the deck-array. All 52 unique card objects, with all their parameters
+// @param: deck (array of Cards)
+// @return: void
+    var deck = createDeck();
+    for (i = 0; i < 52; i++) {
+        setTimeout(function () {
+            console.log(i); 
+            var aCard = deck[i];
+            var x = gameBoard.ctx;
+            x.drawImage(aCard.image, 100,100);
+            console.log(aCard.face + aCard.suit);
+            console.log(aCard.rank + "."+aCard.getCardName() + "."+aCard.image.src);
+        }, 1500);
+    };
 }
 
 function trumpUnitTest(card) {
