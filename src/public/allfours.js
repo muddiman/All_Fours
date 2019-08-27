8b6004803a37622cc46fb932d793fe6240777931
Roger Clarke <rogerclarke00@hotmail.com>
2019-08-27 00:36:58 -0400

    ver. 0.8.8: fixed bug that caused game to hang

--- src/public/allfours.js
+++ src/public/allfours.js
@@ -10,7 +10,7 @@
  *  @author    Roger Clarke (muddiman | .muddicode)
  *  @link      https://www.roger-clarke.com |   https://www.muddicode.com
  *  @email     rogerclarke00@hotmail.com    |   muddiman@hotmail.com  
- *  @version   0.8.6
+ *  @version   0.8.8
  *  @since     2018-10-1
  *  @download  https://www.github.com/muddiman/All_Fours
  *  @license   NOT for 'commercial use'.
