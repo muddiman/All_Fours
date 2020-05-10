3235da241b853beda48acc029f7db40818044606
unknown <rogerclarke00@hotmail.com>
2020-05-10 05:09:00 -0400

    VERSION - 1.0.0

--- scripts/allfours-page-script.js
+++ scripts/allfours-page-script.js
@@ -3,7 +3,15 @@ function usernameHandler(ev) {
     let name = document.getElementById("player_name").value;
     document.getElementById("player_name").value = "";
     console.log(`Player Name: ${name}`);
-    Chat.username = name;
-    sendConnectionMsg();
+    Chat.setUsername(name).init();
+    Chat.sendClientDetails();
     ev.preventDefault();
 }
+
+
+/* 
+    
+    @AUTHOR/PROGRAMMER: muddicode/sauceCode
+    @VERSION: 1.0.0
+
+*/
